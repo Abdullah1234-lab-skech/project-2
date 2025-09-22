b3e8a3f75580fae441c24bda9b69f698032c7a24
Abdullah1234-lab-skech <abdullahhussainn8847@gmail.com>
2025-09-22 11:51:48 +0300

    tip

---
+++ due.py
@@ -0,0 +1,6 @@
+def subtract(amount , amount2): 
+    return amount - amount2
+amount=int(input('Enter a price of an item: ')) 
+amount2=int(input('Enter a price of another item: ')) 
+print("your change is: $",subtract(amount , amount2) ) 
+
